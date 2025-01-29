f040a52cfa40846f0cefbfa7a1311497d6f5e88f
Juan Vaquera <juan.tec@outlook.com>
2025-01-29 17:56:41 -0600

    Finalizado

---
+++ Practica1Clases/MiClase.h
@@ -0,0 +1,16 @@
+//
+//  MiClase.h
+//  Practica1Clases
+//
+//  Created by Juan Vaquera on 29/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MiClase : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Practica1Clases/MiClase.m
@@ -0,0 +1,12 @@
+//
+//  MiClase.m
+//  Practica1Clases
+//
+//  Created by Juan Vaquera on 29/01/25.
+//
+
+#import "MiClase.h"
+
+@implementation MiClase
+
+@end
