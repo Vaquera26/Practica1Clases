85b1c13f345e08eb4be4a950a3b7f271de866584
Juan Vaquera <juan.tec@outlook.com>
2025-01-29 17:23:39 -0600

    Initial Commit

---
+++ Practica1Clases/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Practica1Clases
+//
+//  Created by Juan Vaquera on 29/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
